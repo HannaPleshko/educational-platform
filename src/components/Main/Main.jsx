f34efd467b8728a729700d90c2a374466a864071
Hanna <hannapleshko2000@mail.ru>
2022-01-15 01:10:07 +0300

    start main_section refactoring

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,30 +3,59 @@ import main from './Main.module.css';
 
 const Main = () => {
     return (
-        <section className={classNames(main['gradient'], main['main'])}>
-            <div className={main["mycontain"]}>
-                <div className="flexcolwrap-home-hero">
-                    <div className="flex60col-home-hero">
-                        <div className="home-hero-moving-type">
-                            <h1 className="heading">Online learning platform</h1>
+        <main>
+            <section className={classNames(main['gradient'], main['main'])}>
+                <div className={main["mycontain"]}>
+                    <div className={main["flexcolwrap-home-hero"]}>
+                        <div className={main["flex60col-home-hero"]}>
+                            <div className={main["home-hero-moving-type"]}>
+                                <h1 className={main["heading"]}>Online learning platform</h1>
+                                <h1 className={main["heading"]}>Start your programming career right now!</h1>
+                            </div>
+                            <p className={main["hero-description-spacing"]}>HS is a programming school where we will teach you the knowledge that is in demand today.</p><a href="/" className={main["btn"]}>Get started for free</a></div>
+                        <div className={main["flex40col-home-hero"]}>
+                            <div className={main["main_code"]}></div>
                         </div>
-                        <p className="hero-description-spacing">HS is a programming school where we will teach you the knowledge that is in demand today.</p><a href="/" className="cta-button w-button">Get started for free</a></div>
-                    <div className="flex40col-home-hero">
-                        <div className={main["main_code"]}></div>
-                    </div>
-                    <div className="home-hero-logo-block">
-                        <div className="home-hero-logo-wrap">
-                            {/* <a target="_blank" rel="" href="/"><img src="/static/img/slack-logo.svg" className="home-hero-logo" /></a>
-                                    <a target="_blank" rel="" href="/"><img src="/static/img/github-logo.svg" className="home-hero-logo" /></a>
-                                    <a target="_blank" rel="" href="/"><img src="/static/img/sendgrid-logo.svg" className="home-hero-logo" /></a>
-                                    <a target="_blank" rel="" href="/"><img src="/static/img/twilio-logo.svg" className="home-hero-logo" /></a>
-                                    <a target="_blank" rel="" href="/"><img src="/static/img/atlassian-logo.svg" className="home-hero-logo" /></a> */}
+
+                        <div class={main["head-section__reasons"]}>
+                            <div class={main["head-section__reason"]}>
+                                <div class={main["main_1"]}></div>
+                                <div class={main["head-section__reason-title"]}>
+                                    <span>You can study from anywhere in the world</span>
+                                </div>
+                            </div>
+                            <div class={main["head-section__reason"]}>
+                                <div class={main["main_2"]}></div>
+                                <div class={main["head-section__reason-title"]}>
+                                    <span>CSS, HTML, Java Script, Node.js, React, TypeScript and other important skills</span>
+                                </div>
+                            </div>
+                            <div class={main["head-section__reason"]}>
+                                <div class={main["main_3"]}></div>
+                                <div class={main["head-section__reason-title"]}>
+                                    <span>Practice takes 450 hours out of 560</span>
+                                </div>
+                            </div>
+                            <div class={main["head-section__reason"]}>
+                                <div class={main["main_4"]}></div>
+                                <div class={main["head-section__reason-title"]}>
+                                    <span>Helping you get a job after graduation</span>
+                                </div>
+                            </div>
+                            <div class={main["head-section__reason"]}>
+                                <div class={main["main_5"]}></div>
+                                <div class={main["head-section__reason-title"]}>
+                                    <span>A lot of projects in the portfolio</span>
+                                </div>
+                            </div>
                         </div>
-                        <div className="center">As well as <a href="/" target="_blank" rel="noreferrer noopener" className="inline-link-underline-yellow">Amazon Web Services</a> and many more.</div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <section className={classNames(main['gradient'], main['main'])}>
+
+            </section>
+        </main>
     )
 }
 
