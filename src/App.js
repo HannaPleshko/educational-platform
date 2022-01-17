da307892fdf476feb3ef1e9df3e0ae59b891730a
PLESHKO-H\Admin <HPleshko@ibagroup.eu>
2022-01-17 23:29:26 +0300

    auth_2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 // import Footer from './components/Footer/Footer';
 // import Header from './components/Header/Header'
 // import Main from './components/Main/Main';
--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,29 +1,33 @@
+import classNames from 'classnames';
 import auth from './Auth.module.css';
 
 const Auth = () => {
     return (
-        <div className="">
-            <div className=""></div>
+        <div className={auth["auth"]}>
+            <div className={auth["mycontain"]}>
+                <div className={auth["img_hs_logo"]}>
 
-            <div className="">
-                <div className=""><h2>Log in</h2></div>
-                <div className="">
-                    <p>E-mail</p>
-                    <input type="text"/>
                 </div>
-                <div className="">
-                    <p>Password</p>
-                    <input type="text"/>
-                </div>
-                <div className=""><a>Log in</a></div>
-                <div className=""><a>I forgot my password</a></div>
-                <div className=""><p>or</p></div>
-                <div className=""><a>Log in with GitHub</a></div>
-                <div className=""><a>Log in with Google</a></div>
+                <div className={auth["mycontain-auth"]}>
+                    <div className={auth["auth-logo-h2"]}><h2>Log in</h2></div>
+                    <div className={auth[""]}>
+                        <p>E-mail</p>
+                        <input type="text"/>
+                    </div>
+                    <div className={auth[""]}>
+                        <p>Password</p>
+                        <input type="text"/>
+                    </div>
+                    <div className={auth[""]}><a>Log in</a></div>
+                    <div className={auth[""]}><a>I forgot my password</a></div>
+                    <div className={auth[""]}><p>or</p></div>
+                    <div className={auth[""]}><a>Log in with GitHub</a></div>
+                    <div className={auth[""]}><a>Log in with Google</a></div>
 
+                </div>
             </div>
         </div>
-)
+    )
 }
 
 export default Auth
