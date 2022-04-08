1ae5d7b9588247c81c57c8cb475e5b441d769795
Vadimchesh <vadimlindo@gmail.com>
2022-04-08 10:52:43 +0400

    feat: add request to login

--- src/redux/authApi.js
+++ src/redux/authApi.js
@@ -1,18 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const authApi = createApi({
-    reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
-    endpoints: builder => ({
-        getAccessToken: builder.query({
-            query: (url, body) => ({
-                url: url,
-                method: 'POST',
-                body: { body }
-            }),
-            transformResponse: (response) => response,
-        }),
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: '/api/auth',
+        method: 'POST',
+        body: credentials,
+      }),
     }),
+  }),
 });
 
-export const { useGetAccessTokenQuery } = authApi
+export const { useLoginMutation } = authApi;
