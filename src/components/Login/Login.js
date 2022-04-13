1870667b4da9955a729a395b5a02896a93d57222
PLESHKO-H\Admin <hannapleshko2000@mail.ru>
2022-04-13 20:08:03 +0300

    navbar NavListLessons

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,42 +1,44 @@
-import React, { useState, useContext } from "react";
+import { useState, useContext } from "react";
 import { Link, useNavigate } from 'react-router-dom';
-import login from './Login.module.css';
+import style from './Login.module.css';
 import { AuthContext } from "../../context/AuthContext"
 import { TextField } from '@material-ui/core';
 import { FormControl, Input, InputLabel, InputAdornment, IconButton, Button } from '@mui/material';
-import {Visibility, VisibilityOff} from '@mui/icons-material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { useLoginMutation } from '../../redux';
 import Loader from '../Loader/Loader';
 
 const Login = () => {
-  const navigate = useNavigate();
-  const auth = useContext(AuthContext);
+    const navigate = useNavigate();
+    const auth = useContext(AuthContext);
 
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
-  });
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    });
 
-  const changeForm = (event) => {
-    setForm({ ...form, [event.target.name]: event.target.value });
-  };
-  const [login, { data, isLoading, isSuccess }] = useLoginMutation();
-
-  if (isLoading) {
-    return <Loader />;
-  }
-
-  if (isSuccess) {
-    auth.login(data.token);
-  }
-
-    const [values, setValues] = React.useState({
+    const [values, setValues] = useState({
         amount: '',
         password: '',
         weight: '',
         weightRange: '',
         showPassword: false,
     });
+
+    const changeForm = (event) => {
+        setForm({ ...form, [event.target.name]: event.target.value });
+    };
+
+    const [login, { data, isLoading, isSuccess }] = useLoginMutation();
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    if (isSuccess) {
+        auth.login(data.token);
+    }
+
 
     const handleChange = (prop) => (event) => {
         setValues({ ...values, [prop]: event.target.value });
@@ -55,11 +57,11 @@ const Login = () => {
     };
 
     return (
-        <div className={login["login"]}>
-            <div className={login["content"]}>
-                <div className={login["mycontain"]}>
-                    <div className={login["block-logo-login"]}><h2>Log In</h2></div>
-                    <div className={login["block-login"]}>
+        <div className={style["login"]}>
+            <div className={style["content"]}>
+                <div className={style["mycontain"]}>
+                    <div className={style["block-logo-login"]}><h2>Log In</h2></div>
+                    <div className={style["block-login"]}>
                         <TextField name='email' onChange={changeForm} type='text' label="Email" />
 
                         {/* <TextField name='password' onChange={changeForm} type='password' label="Password" /> */}
@@ -86,25 +88,25 @@ const Login = () => {
                         </FormControl>
 
                     </div>
-                    <div className={login["block-bottom"]}>
-                        <p className={login["mycontain--login"]}>Forgot password?
-                            <Link to={"*"} className={login["mycontain--login-link"]}><Button>Click Here</Button></Link>
+                    <div className={style["block-bottom"]}>
+                        <p className={style["mycontain--login"]}>Forgot password?
+                            <Link to={"*"} className={style["mycontain--login-link"]}><Button>Click Here</Button></Link>
                         </p>
 
-                            <Button variant="contained"
-                                    onClick={async () => {
-                                        try {
-                                            const result = await login(form);
-                                            if (result.data) {
-                                                navigate('/course');
-                                            }
-                                        } catch (err) {
-                                            console.log(err);
-                                        }
-                                    }}
-                            >Log In</Button>
+                        <Button variant="contained"
+                            onClick={async () => {
+                                try {
+                                    const result = await login(form);
+                                    if (result.data) {
+                                        navigate('/course');
+                                    }
+                                } catch (err) {
+                                    console.log(err);
+                                }
+                            }}
+                        >Log In</Button>
                     </div>
-                    <p className={login["mycontain--reg"]}>Don't have an account?<Link to={"/register"} className={login["mycontain--reg-link"]}><Button>Register</Button></Link>
+                    <p className={style["mycontain--reg"]}>Don't have an account?<Link to={"/register"} className={style["mycontain--reg-link"]}><Button>Register</Button></Link>
                     </p>
                 </div>
             </div>
--- src/pages/LessonPage/LessonPage.js
+++ src/pages/LessonPage/LessonPage.js
@@ -1,7 +1,7 @@
 import { useState, useCallback, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import DoTask from '../../components/DoTask/DoTask';
-import Header from '../../components/Header/Header';
+// import Header from '../../components/Header/Header';
 import NavListLessons from '../../components/NavListOfLessons/NavListLessons';
 import Loader from '../../components/Loader/Loader';
 import lesson from './css/LessonPage.module.css'
@@ -16,27 +16,27 @@ const LessonPage = () => {
   let { data: lessons, isLoading, isError } = useGetLessonsQuery(`/lesson/${courseId}/${topicId}`)
   const { data: topics } = useGetTopicsQuery(`/topic/${courseId}`)
 
-  let titles = [
-    {
-      id: Math.random() * 1000,
-      title: 'Why HS?'
-    },
-    {
-      id: Math.random() * 1000,
-      title: 'Courses'
-    },
-    {
-      id: Math.random() * 1000,
-      title: 'Literature'
-    },
-    {
-      id: Math.random() * 1000,
-      title: 'WorkSpace'
-    },
-    {
-      id: Math.random() * 1000,
-      title: 'Contacts'
-    }]
+  // let titles = [
+  //   {
+  //     id: Math.random() * 1000,
+  //     title: 'Why HS?'
+  //   },
+  //   {
+  //     id: Math.random() * 1000,
+  //     title: 'Courses'
+  //   },
+  //   {
+  //     id: Math.random() * 1000,
+  //     title: 'Literature'
+  //   },
+  //   {
+  //     id: Math.random() * 1000,
+  //     title: 'WorkSpace'
+  //   },
+  //   {
+  //     id: Math.random() * 1000,
+  //     title: 'Contacts'
+  //   }]
 
   let lastLessonIndex, firstLessonIndex, currentLesson, paginate;
   if (lessons && !isError) {
@@ -53,7 +53,6 @@ const LessonPage = () => {
 
   return (
     <div>
-      <Header titles={titles}></Header>
       <div className={lesson['flex-content']}>
         {topics ? <NavListLessons topics={topics} /> : null}
         {lessons ? <DoTask arrLength={lessons.length} currentLesson={currentLesson} lessonsPerPage={lessonsPerPage} paginate={paginate} /> : <h1>Для данной темы пока что нет уроков</h1>}
@@ -63,3 +62,4 @@ const LessonPage = () => {
 };
 
 export default LessonPage;
+
